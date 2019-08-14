aab18cbe397f39d482739ec78a6dac30c16dd4c4
YaYII <yangying19911113@163.com>
2019-08-14 09:58:41 +0800

    Create indexObject.js

---
+++ indexObject.js
@@ -0,0 +1,55 @@
+//声明返回按钮对象
+var host= {
+	id : "host",
+	title : "返回",
+	left : 10,
+	top : 10
+};
+//声明确定按钮对象
+var enter = {
+	id:"enter",
+	title:"确认",
+	left : 140,
+	top : 10
+};
+//声明确定按钮对象 
+var enter1 = {
+	id:"enter",
+	title:"确认",
+	left : 270,
+	top : 10
+};
+//声明确定按钮对象
+var enter2 = {
+	id:"enter",
+	title:"确认",
+	left : 400,
+	top : 10
+};
+//声明确定按钮对象
+var enter3 = {
+	id:"enter",
+	title:"确认",
+	left : 530,
+	top : 10
+
+};
+
+//声明产品1对象
+var div1 = {
+	id:"div1",
+	title:"产品1",
+	left : 10,
+	top : 60
+	
+	
+};
+//声明产品2对象
+var div2 = {
+	id:"div2",
+	title:"产品2",
+	left : 140,
+	top : 60
+	
+};
+
